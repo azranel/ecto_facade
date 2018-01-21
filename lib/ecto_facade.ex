79149c43033dcea2bf15e2cc8c046c33c5700298
Bartosz Łęcki <chunkyo@gmail.com>
2018-01-21 18:10:55 +0100

    Remove dead code

--- lib/ecto_facade.ex
+++ lib/ecto_facade.ex
@@ -1,18 +1,2 @@
 defmodule EctoFacade do
-  @moduledoc """
-  Documentation for EctoFacade.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EctoFacade.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
